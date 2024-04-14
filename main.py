89fb4008a2726c059e3d289847041233b70dff8e
youknew <namyoonwoo560@gmail.com>
2024-04-14 20:11:27 +0900

    complete

---
+++ main.py
@@ -0,0 +1,17 @@
+from get_date_diff import get_date_diff
+from datetime import date
+
+# input pages amount
+while True:
+    try:
+        pages = int(input("how many pages you to do : "))
+        break
+    except:
+        print("다시 입력해주세용")
+
+date_diff = get_date_diff(date.today().strftime("%m"), date.today().strftime("%d"))
+
+amount = pages//date_diff
+
+if __name__ == "__main__":
+    print("{}장".format(amount))
